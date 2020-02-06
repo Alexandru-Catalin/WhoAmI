46693d2dd0de65ad1e1680e673f4f8382e3d0eeb
(s) Alexandru-Catalin Marcu <amarcu>
2020-02-06 14:09:25 +0000

    Starting

---
+++ WhoAmI/Assets/Scripts/characterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour {
+
+    public float speed = 30.0F;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+		
+	}
+}
