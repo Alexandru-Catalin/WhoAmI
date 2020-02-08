8f7f0dedd552c97f559fb5f005d5738a263792b2
(s) Alexandru-Catalin Marcu <amarcu>
2020-02-08 16:32:48 +0000

    Teleport + logo

---
+++ WhoAmI/Assets/Scripts/portalTeleporter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalTeleporter : MonoBehaviour
+{
+
+    public Transform Player;
+    public Transform reciever;
+
+    private bool playerIsOverlapping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = Player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if (dotProduct < 0f)
+            {
+                //Teleport him!
+                float rotationDiff = Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                Player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                Player.position = reciever.position + positionOffset;
+
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // if player z position is behind our door,
+            // return
+
+            // else, do this
+            playerIsOverlapping = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
---
+++ WhoAmI/Assets/Scripts/portalTextureSetup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalTextureSetup : MonoBehaviour
+{
+
+    public Camera camera_B;
+    public Camera camera_A;
+    public Material cameraMat_B;
+    public Material cameraMat_A;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (camera_A.targetTexture != null)
+        {
+            camera_A.targetTexture.Release();
+        }
+        camera_A.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMat_A.mainTexture = camera_A.targetTexture;
+
+        if (camera_B.targetTexture != null)
+        {
+            camera_B.targetTexture.Release();
+        }
+        camera_B.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMat_B.mainTexture = camera_B.targetTexture;
+
+    }
+
+
+}
