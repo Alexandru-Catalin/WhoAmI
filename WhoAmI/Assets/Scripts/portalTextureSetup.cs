186f25e8085c603ee89ba61dc1ce5ceb80eaa0b2
(s) Alexandru-Catalin Marcu <amarcu>
2020-02-10 15:43:02 +0000

    test

--- WhoAmI/Assets/Scripts/portalTextureSetup.cs
+++ WhoAmI/Assets/Scripts/portalTextureSetup.cs
@@ -6,20 +6,13 @@ public class portalTextureSetup : MonoBehaviour
 {
 
     public Camera camera_B;
-    public Camera camera_A;
+
     public Material cameraMat_B;
-    public Material cameraMat_A;
+   
 
     // Use this for initialization
     void Start()
     {
-
-        if (camera_A.targetTexture != null)
-        {
-            camera_A.targetTexture.Release();
-        }
-        camera_A.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMat_A.mainTexture = camera_A.targetTexture;
 
         if (camera_B.targetTexture != null)
         {
