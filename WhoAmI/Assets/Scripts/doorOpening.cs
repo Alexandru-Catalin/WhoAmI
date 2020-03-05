a8f76455eae6bc0c98588312d3cde20c44f4b560
(s) Alexandru-Catalin Marcu <alexandrumarcu479@gmail.com>
2020-03-05 13:23:36 +0000

    DoorAnimations

---
+++ WhoAmI/Assets/Scripts/doorOpening.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpening : MonoBehaviour
+{
+
+    private Animator anim;
+    private bool inRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange)
+        {
+            if (!anim.GetBool("Open"))
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    anim.SetBool("Open", true);
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.SetBool("Open", false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        inRange = true;
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        inRange = false;
+    }
+}
