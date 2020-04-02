9c6e6352331d7d84d231ec143d590c6c91b8d8ff
unknown <alexandrumarcu479@gmail.com>
2020-04-02 17:55:02 +0100

    fixing bugs

--- WhoAmI/Assets/Scripts/openLater.cs
+++ WhoAmI/Assets/Scripts/openLater.cs
@@ -6,7 +6,7 @@ public class openLater : MonoBehaviour
 {
 
     private Animator anim;
-    public float timer = 10f;
+    public float timer = 10;
     
     // Start is called before the first frame update
     void Start()
