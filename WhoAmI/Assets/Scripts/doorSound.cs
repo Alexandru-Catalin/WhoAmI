093455c80554cccfd8243605523f076f6133b0fb
unknown <alexandrumarcu479@gmail.com>
2020-05-18 15:07:47 +0100

    DoorSound

---
+++ WhoAmI/Assets/Scripts/doorSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorSound : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+    public bool played = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        played = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (played == false)
+            {
+                audioSource.Play();
+                played = true;
+            }
+
+        }
+    }
+}
